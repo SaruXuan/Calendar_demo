4444a4de1a469469e5eb9ed55655ee52639be341
SaruXuan <38649514+SaruXuan@users.noreply.github.com>
2020-09-20 14:31:23 +0800

    20200920

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,9 @@
 <?php include('header.php') ?>
 <?php include('data.php') ?>
 
-<div id="calender">
+<div id="calender" data-year="<?= date('Y') ?>" data-month="<?= date('n') ?>">
     <div id="header">
-        <?= date('Y') ?> / <?= date('m') ?>
+        <?= date('Y') ?> / <?= date('n') ?>
     </div>
     <div id="days">
         <?php foreach ($days as $key => $value): ?>
@@ -12,23 +12,28 @@
     </div>
     <div id="dates">
         <?php foreach ($dates as $key => $value): ?>
-            <div class="date-block <?= (is_null($value)) ? 'empty':'' ?>">
+            <div class="date-block <?= (is_null($value)) ? 'empty':'' ?>" data-date="<?= $value ?>">
                 <div class="date"><?= $value ?></div>
-                <div class="event"></div>
+                <div class="events">
+                    <div class="event clearfix">
+                        <div class="from">10:00</div>
+                        <div class="title">title</div>
+                    </div>
+                </div>
             </div>
         <?php endforeach ?>
     </div>
 </div>
 
-<div id="info-panel" class="new">
+<div id="info-panel">
     <div class="close">x</div>
     <div class="title">
         <label>Event</label>
-        <div contenteditable="true"></div>
+        <div class='event-input' contenteditable="true"></div>
     </div>
     <div class="time-picker">
         <div class="selected-date">
-            <span class="month">09</span>/<span class="date">13</span>
+            <span class="month"></span>/<span class="date"></span>
         </div>
         <div class="from">
             <label for="from">From</label><br>
--- public_html/js/action.js
+++ public_html/js/action.js
@@ -1,8 +1,90 @@
 $(document).ready(function(){
+
+    //將事件處理包成dic
+    var panel = {
+        el: '#info-panel',
+        open: function(isNew, e) {
+            //讓事件視窗出現在點擊附近而且不會超出母視窗
+            var midwidth = $(window).width()/2;
+            var onethirdheight = $(window).height()/3;
+            var twothirdheight = $(window).height()*2/3;
+            var clickobj;
+            if (e.pageX > midwidth && e.pageY > twothirdheight) {
+                clickobj = $(panel.el).addClass('open').css({
+                    top: (e.pageY-380)+'px',
+                    left: (e.pageX-250)+'px'
+                });
+            } else if (e.pageX > midwidth && e.pageY <= twothirdheight && e.pageY > onethirdheight) {
+                clickobj = $(panel.el).addClass('open').css({
+                    top: (e.pageY-190)+'px',
+                    left: (e.pageX-250)+'px'
+                });
+            } else if (e.pageX > midwidth && e.pageY <= onethirdheight) {
+                clickobj = $(panel.el).addClass('open').css({
+                    top: e.pageY+'px',
+                    left: (e.pageX-250)+'px'
+                });
+            } else if (e.pageX <= midwidth && e.pageY > twothirdheight) {
+                clickobj = $(panel.el).addClass('open').css({
+                    top: (e.pageY-380)+'px',
+                    left: e.pageX+'px'
+                });
+            } else if (e.pageX <= midwidth && e.pageY <= twothirdheight && e.pageY > onethirdheight) {
+                clickobj = $(panel.el).addClass('open').css({
+                    top: (e.pageY-190)+'px',
+                    left: e.pageX+'px'
+                });
+            } else {
+                clickobj = $(panel.el).addClass('open').css({
+                    top: e.pageY+'px',
+                    left: e.pageX+'px'
+                });
+            }
+            //標上日期
+            panel.updateDate(e);
+            //打開事件視窗時直接讓title可編輯
+            clickobj.find('.event-input').focus();
+            //判斷是新開還是更新
+            if (isNew) {
+                $(panel.el).addClass('new').removeClass('update');
+            } else {
+                $(panel.el
+                    ).addClass('update').removeClass('new');
+            }
+        },
+        close: function() {
+            $(panel.el).removeClass('open');
+        },
+        updateDate: function(e) {
+            //從date-block取得日期
+            if ($(e.currentTarget).is('.date-block')) {
+                var date = $(e.currentTarget).data('date');
+            } else {
+                var date = $(e.currentTarget).closest('.date-block').data('date');
+            }
+            //從#calender取得月
+            var month = $('#calender').data('month');
+            //印出
+            $(panel.el).find('.month').text(month);
+            $(panel.el).find('.date').text(date);
+        }
+    };
+
     //雙擊跳出事件視窗
     $('.date-block').on('dblclick', function(e){
-        $('#info-panel').addClass('open');
+        //呼叫panel的open函式
+        panel.open(true, e);
     });
+
+    
+    $('.date-block').on('dblclick', '.event', function(e){
+        //停止event bubbling
+        e.stopPropagation();
+        //呼叫panel的open函式
+        panel.open(false, e);
+    });
+
+
     //點擊按鈕觸發事件
     $('#info-panel')
         .on('click', 'button', function(e){
@@ -13,7 +95,7 @@ $(document).ready(function(){
 
             }
             if ($(this).is('.cancel')) {
-                $('#info-panel').removeClass('open');
+                panel.close();
             }
             if ($(this).is('.delete')) {
 
@@ -21,7 +103,7 @@ $(document).ready(function(){
         })
     //點擊叉叉關閉事件視窗
         .on('click', '.close', function(e){
-            $('button.cancel').click();
+            panel.close();
     });
 });
 
